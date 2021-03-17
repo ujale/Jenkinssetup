85da735e164f3d73f93e158a7226cdc04932698d
ujale <Udeme.Bassey@VentureGardenGroup.Local>
2021-03-17 21:40:10 +0100

    orbit on prod

--- cypress/integration/Login.spec.js
+++ cypress/integration/Login.spec.js
@@ -2,11 +2,11 @@
 
 describe ('test jenkins project', function () {
     it ('Login to Orbit test web', function() {
-        cy.visit('http://orbitclientui.test.vggdev.com', { retryOnNetworkFailure: true });
+        cy.visit('http://orbit.vigipay.co/', { retryOnNetworkFailure: true });
         cy.wait(3000)
         cy.contains('Sign In').click()
-        cy.get(':nth-child(1) > .withIcon > .form-control').type("pontius@yopmail.com")
-        cy.get(':nth-child(2) > .withIcon > .form-control').type("P0ntius.")
+        cy.get(':nth-child(1) > .withIcon > .form-control').type("carison@yopmail.com")
+        cy.get(':nth-child(2) > .withIcon > .form-control').type("P@ssw0rd")
         cy.get(".btn").click({force:true})
         cy.wait(12000)
         
